a40130b48bd96b67acf098ea9082449fd3be3e20
James E. King III <jeking3rd@gmail.com>
2018-10-01 13:55:55 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[Issue](https://github.com/StackStorm/st2/issues/4368)
---
+++ actions/get-path.py
@@ -0,0 +1,7 @@
+from st2common.runners.base_action import Action
+
+
+class GetPath(Action):
+
+    def run(self, path):
+        return path
